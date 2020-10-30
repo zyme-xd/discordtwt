ecf70254aff23349cda2d3a535609a015165f91a
jylescoad-ward <jyles@dariox.club>
2020-10-30 13:24:48 +0800

    moved all configs here

--- config.js
+++ config.js
@@ -3,5 +3,14 @@ module.exports = {
     "sendCooldown": 10000,
     "errCooldown":5000,
     "logChannel":"channelid",
-    "admins": ["admin1","admin2"]
+    "admins": ["admin1","admin2"],
+    "token": {
+        "twitter": {
+            "consumer_key": process.env.CONSUMER_KEY,
+            "consumer_secret": process.env.CONSUMER_SECRET,
+            "access_token": process.env.ACCESS_TOKEN,
+            "access_token_secret": process.env.ACCESS_SECRET
+        },
+        "discord": process.env.DISCORD_TOKEN
+    }
 }
