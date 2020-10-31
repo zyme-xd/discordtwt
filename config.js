1743a01433cd041f2357239c19357f699e69d23f
Jyles Coad-Ward <jyles@dariox.club>
2020-10-31 15:08:56 +0800

    env var or string

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ module.exports = {
     "admins": ["admin1","admin2"],
     "token": {
         "twitter": {
-            "consumer_key": process.env.CONSUMER_KEY,
-            "consumer_secret": process.env.CONSUMER_SECRET,
-            "access_token": process.env.ACCESS_TOKEN,
-            "access_token_secret": process.env.ACCESS_SECRET
+            "consumer_key": process.env.CONSUMER_KEY || "",
+            "consumer_secret": process.env.CONSUMER_SECRET || "",
+            "access_token": process.env.ACCESS_TOKEN || "",
+            "access_token_secret": process.env.ACCESS_SECRET || ""
         },
-        "discord": process.env.DISCORD_TOKEN
+        "discord": process.env.DISCORD_TOKEN || ""
     }
 }
